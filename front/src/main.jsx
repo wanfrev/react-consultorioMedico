0fc87769bbc5a83680f057ebda07ac3ded5b0282
Wanfredo Vargas <wanfrevargas@gmail.com>
2024-10-23 17:35:56 -0400

    Ahora si

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './html/Home';
-import Inicio from './html/Inicio';
+import { BrowserRouter } from 'react-router-dom';
+import { MedicalApp } from './MedicalApp';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/inicio" element={<Inicio />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <MedicalApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
